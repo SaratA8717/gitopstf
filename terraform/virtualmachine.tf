0d5ec28bcb551fe71114097d8eb01abab57d78b4
Sarat A <sarat.ankala@outlook.com>
2024-04-21 19:18:40 +0530

    Updated TF code for VM & VNET
    Created code for virtual machine deployment & Virtual Network Deployment

---
+++ terraform/virtualmachine.tf
@@ -0,0 +1,39 @@
+resource "azurerm_resource_group" "azvmrg" {
+  name     = "azvm-rg"
+  location = "east us"
+}
+
+resource "azurerm_network_interface" "appvm1nic" {
+  name                = "appvm1nic"
+  location            = azurerm_resource_group.azvmrg.location
+  resource_group_name = azurerm_resource_group.azvmrg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm-snet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "appvm1nic" {
+  name                = "appvm1"
+  resource_group_name = azurerm_resource_group.azvmrg.name
+  location            = azurerm_resource_group.azvmrg.location
+  size                = "Standard_B2ms"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.appvm1nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
---
+++ terraform/virtualnetwork.tf
@@ -0,0 +1,25 @@
+
+resource "azurerm_virtual_network" "hub-vnet" {
+  name                = "hub-vnet"
+  location            = azurerm_resource_group.core-rg.location
+  resource_group_name = azurerm_resource_group.core-rg.name
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    environment = "Production"
+  }
+}
+
+
+resource "azurerm_subnet" "app-snet" {
+  name                 = "appsnet1"
+  resource_group_name  = azurerm_resource_group.core-rg.name
+  virtual_network_name = azurerm_virtual_network.hub-vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "vm-snet" {
+  name                 = "vmssnet"
+  resource_group_name  = azurerm_resource_group.core-rg.name
+  virtual_network_name = azurerm_virtual_network.hub-vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
