3178dd1670d3ebdb638fc88482b1e35817a63104
Sarat A <sarat.ankala@outlook.com>
2024-04-21 19:21:12 +0530

    Updated Virtual network resource group

--- terraform/virtualnetwork.tf
+++ terraform/virtualnetwork.tf
@@ -1,8 +1,8 @@
 
 resource "azurerm_virtual_network" "hub-vnet" {
   name                = "hub-vnet"
-  location            = azurerm_resource_group.core-rg.location
-  resource_group_name = azurerm_resource_group.core-rg.name
+  location            = azurerm_resource_group.azvmrg.location
+  resource_group_name = azurerm_resource_group.azvmrg.name
   address_space       = ["10.0.0.0/16"]
   tags = {
     environment = "Production"
@@ -12,14 +12,14 @@ resource "azurerm_virtual_network" "hub-vnet" {
 
 resource "azurerm_subnet" "app-snet" {
   name                 = "appsnet1"
-  resource_group_name  = azurerm_resource_group.core-rg.name
+  resource_group_name  = azurerm_resource_group.azvmrg.name
   virtual_network_name = azurerm_virtual_network.hub-vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_subnet" "vm-snet" {
   name                 = "vmssnet"
-  resource_group_name  = azurerm_resource_group.core-rg.name
+  resource_group_name  = azurerm_resource_group.azvmrg.name
   virtual_network_name = azurerm_virtual_network.hub-vnet.name
   address_prefixes     = ["10.0.3.0/24"]
 }
