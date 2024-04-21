3e88b9d9e32dd478bddc002c14c8b9eab2f0708b
Sarat A <sarat.ankala@outlook.com>
2024-04-21 16:57:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 3.0.0"
+  required_version = ">= 1.5.7"
   backend "azurerm" {
     resource_group_name  = "tfstate8739"
     storage_account_name = "tfstor9839"
